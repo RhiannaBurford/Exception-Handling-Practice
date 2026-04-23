570d22c8bebfa5ce4c9ea404129b7bc33d279b76
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-23 19:35:26 +0100

    Add finally block at the end of try-catch

---
+++ InvalidPasswordException.java
@@ -0,0 +1,8 @@
+import java.lang.Exception;
+
+public class InvalidPasswordException extends Exception{
+    
+    public InvalidPasswordException(String message){
+        super(message);
+    }
+}
---
+++ RegistrationManager.java
@@ -0,0 +1,19 @@
+public class RegistrationManager {
+
+    public void checkPassword(String password) throws InvalidPasswordException{
+        if (password.length() < 8){
+            throw new InvalidPasswordException("Password is not long enough");
+        }
+        System.out.println("Password accepted");
+    }
+
+    public void registerUser(){
+        String input = "12345";
+        try{
+            checkPassword(input);
+        }
+        catch (InvalidPasswordException e){
+            System.out.println("ERROR: " + e.getMessage() + ". Please try a longer password");
+        }
+    }
+}
